12a5d286d643106ba9904b61b72df32a8af368ba
Caleb Oglesby <noobyman68@gmail.com>
2025-07-24 22:28:08 -0400

    rewrote oglesby_server.c for windows

--- oglesby_server.c
+++ oglesby_server.c
@@ -1,101 +1,114 @@
-#include <sys/socket.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <stdint.h>
-#include <stdbool.h>
 #include <stdio.h>
+#include <stdbool.h>
 #define PORT 5000
-#define fail() exit(EXIT_FAILURE)
 
-char* reverseStr(char *arrIn, int len){
-  char *arrNew = malloc(len); //sizeof(char) = 1
-  for(int i = len-1; i > -1; i--){
-    *arrNew = arrIn[i];
-    arrNew++;
+#pragma comment(lib, "ws2_32.lib")
+
+
+char* reverse(char *arr, int size){
+  char *reversed = malloc(size);
+  for(int i = size-1; i > -1; i--){
+    *reversed = arr[i];
+    reversed++;
   }
-  //need to reset arrNew to first address of arr
-  return arrNew-len;
-}
+  return reversed - size;
+ }
 
-int strEquals(char *arr){
+
+bool endCheck(char *arr){
   char check[3] = {'d','n','e'};
   for(int i = 0; i < 3; i++){
-    if(arr[i] != check[i]){
-      return 0;
+    if(check[i] != arr[i]){
+      return false;
     }
   }
-  return 1;
+  return true;
 }
 
 int main(){
-  int server_fd, new_socket;
-  ssize_t byteNum;
-  struct sockaddr_in address;
-  int opt = 1; 
-  socklen_t addrlen = sizeof(address);
+  WSADATA wsaData;
+  SOCKET s_socket = INVALID_SOCKET, c_socket = INVALID_SOCKET;
+  struct sockaddr.in serverAddr, clientAddr;
+  int clientAddrLen = sizeof(clientAddr);
+  int size;
 
-//socket functions wrapped in if statements for error checking
-
-//creates IPv4, TCP socket
-  if((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
-    perror("socket creation failed");
-    fail();
+  if (WSAStartup(MAKEWORD(2,2), &wsaData)!= 0){
+    printf("WSAStartup failed");
+    return 1;
   }
 
-//tells server to occasionally check if connection is still alive  
-  if(setsockopt(server_fd, SOL_SOCKET, SO_KEEPALIVE, &opt, sizeof(opt)) < 0){
-    perror("setsockopt failed");
-    fail();
+  if((s_socket = socket(AF_INET, SOCKSTREAM, IPPROTO_TCP)) == INVALID_SOCKET){
+    printf("socket creation failed");
+    WSACleanup();
+    return 1;
+  }
+
+  serverAddr.sin_family = AF_INET;
+  serverAddr.sin_addr.s_addr = INADDR_ANY;
+  serverAddr.sin_port = htons(PORT);
+
+  if(bind(s_socket, (struct sockaddr*) &serverAddr, sizeof(serverAddr)) == SOCKET_ERROR){
+    printf("binding failed");
+    closesocket(s_socket);
+    WSACleanup();
+    return 1;
+  }
+
+  if(listen(s_socket, 3) == SOCKET_ERROR){
+    printf("listen failed");
+    closesocket(s_socket);
+    WSACleanup();
+    return 1;
   }
   
-  address.sin_family = AF_INET; //sets communication to IPv4 to match socket
-  address.sin_addr.s_addr = INADDR_ANY; //allows any IP address to connect
-  address.sin_port = htons(PORT); //sets port
-
-//attaches socket to port
-  if(bind(server_fd, (struct sockaddr*)&address, addrlen) < 0){
-    perror("binding failed");
-    fail();
+  if((c_socket = accept(s_socket, (struct sockaddr) &clientAddr, &clientAddrLen)) == INVALID_SOCKET){
+    printf("accept failed");
+    closesocket(s_socket);
+    closesocket(c_socket);
+    WSACleanup();
+    return 1;
   }
-
-//tells socket to wait for connection attempt
-  if(listen(server_fd, 2) < 0){
-    perror("listen failed");
-    fail();
-  }
-
-
-  if((new_socket = accept(server_fd, (struct sockaddr*)&address, &addrlen)) < 0){
-    perror("connection failed");
-    fail();
-  }
-
-  uint8_t msg_len, total;
+  
   while(true){
-    //first message is size of data for next message
-    recv(new_socket, &msg_len, sizeof(msg_len), 0);
-    //msg_len = ntohl(len); //ntohl() converts byte order from network to host
-
-    //msg_len should only be message
-    char *buffer = malloc(msg_len);
-    while(total < msg_len){
-      byteNum = recv(new_socket, buffer + total, msg_len - total,0);
-      total += byteNum;
-    } 
-    char *revStr = reverseStr(buffer, msg_len);
-    free(buffer);
-    send(new_socket, revStr, msg_len, 0);
-    if(msg_len == 3 && strEquals(revStr)){
-      free(revStr);
-      break;
+    if((recSize = recv(clientSocket, size, 4,0)) < 0){
+      printf("size recieve failed");
+      closesocket(s_socket);
+      closesocket(c_socket);
+      WSACleanup();
+      return 1;
     }
-    free(revStr);
-    revStr = NULL;
+    char *buffer = malloc(size);
+    if((recSize = recv(c_socket, buffer, size, 0)) < 0){
+      printf("message recieve failed");
+      closesocket(s_socket);
+      closesocket(c_socket);
+      WSACleanup();
+      return 1;
+    }
+    char *reversed = reverse(buffer, size);
+    if((iResult = send(c_socket, reversed, size, 0)) == SOCKET_ERROR){
+      printf("send failed");
+      closesocket(s_socket);
+      closesocket(c_socket);
+      WSACleanup();
+      free(buffer);
+      return 1;
+    }
+    if(size == 3 && endCheck(reversed)){
+      closesocket(s_socket);
+      closesocket(c_socket);
+      WSACleanup();
+      free(buffer);
+      free(reversed);
+      return 0;
+    }
+    free(buffer);
+    free(reversed);
+    reversed = NULL;
     buffer = NULL;
   }
-  close(server_fd);
-  close(new_socket);
-  return 0;
+
 }
